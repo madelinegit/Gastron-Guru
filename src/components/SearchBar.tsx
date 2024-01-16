397dd2fe4b50a6c7e59883c8f52ee6d8c50d7032
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-01-16 20:18:57 -0300

    feat: add logic to the typing animation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,26 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function SearchBar() {
   const inputRef = useRef(null);
   const [placeholder, setPlaceholder] = useState<string>('');
+
+  const stringToSplit: string = 'Search chefs in your location';
+
+  useEffect(() => {
+    const splittedString: string[] = stringToSplit.split('');
+    let index: number = -1;
+
+    const timer = setInterval(() => {
+      if (index < splittedString.length) {
+        index++;
+        if (splittedString[index] != undefined) {
+          setPlaceholder((prevText) => prevText + splittedString[index]);
+        }
+      }
+    }, 300);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div>
