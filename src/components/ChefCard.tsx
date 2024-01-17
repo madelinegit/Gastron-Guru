7a4a861be61400d9fc8840c89d760921f0361956
Demi Hayashi <demi.h@me.com>
2024-01-17 15:15:03 -0800

    update css according to mockup

--- src/components/ChefCard.tsx
+++ src/components/ChefCard.tsx
@@ -7,7 +7,7 @@ const Card = () => {
   return (
     <div className="chef-card-container">
       {chefsData.map((chef) => (
-        <div className="chef-card">
+        <div className="chef-card shadow">
           <div className="thumbnail">
             {/* FILLER IMAGE */}
             <img src="https://images.unsplash.com/photo-1630445396366-8dea03c85ead?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Peter Winston" />
