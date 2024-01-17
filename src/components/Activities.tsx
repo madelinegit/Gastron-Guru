201254b3c5ca907c328449623989a9bf28873b4e
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-01-17 15:12:42 -0300

    feat: populate component using props

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -1,3 +1,32 @@
-export const Activities = () => {
-  return <div>Activities</div>;
+type ActivitiesProps = {
+  image: string;
+  altText: string;
+  title: string;
+  description?: string;
+  inverted: boolean;
 };
+
+const Activities = ({
+  image,
+  altText,
+  title,
+  description,
+  inverted,
+}: ActivitiesProps) => {
+  return (
+    <div
+      className={
+        inverted! ? 'activities-container inverted' : 'activities-container'
+      }
+    >
+      <img src={image} alt={altText} className="img" />
+      <div className="description">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button>View</button>
+      </div>
+    </div>
+  );
+};
+
+export default Activities;
