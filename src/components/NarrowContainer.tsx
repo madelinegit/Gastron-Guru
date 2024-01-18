814e05308d03006c5d2f90aa56d5a2f485a0d76d
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-01-18 15:35:23 -0300

    refactor: using className to stylize

--- src/components/NarrowContainer.tsx
+++ src/components/NarrowContainer.tsx
@@ -1,9 +1,11 @@
+import './NarrowContainer.scss';
+
 type NarrowContainerProps = {
   children: React.ReactNode;
 };
 
 const NarrowContainer = ({ children }: NarrowContainerProps) => {
-  return <div>{children}</div>;
+  return <div className="narrow-container">{children}</div>;
 };
 
 export default NarrowContainer;
