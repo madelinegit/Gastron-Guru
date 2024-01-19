713c1c82d9c370e30c48b455a06030f8af98f88a
madelinegit <120407458+madelinegit@users.noreply.github.com>
2024-01-19 15:20:11 -0600

    re-forked

--- src/components/RelativeSquareBlocks.tsx
+++ src/components/RelativeSquareBlocks.tsx
@@ -1,22 +1,30 @@
-import "./RelativeSquareBlocks.scss"
-
+import "./RelativeSquareBlocks.scss";
 
 function RelativeSquareBlocks() {
   return (
-    <div className = "parent">
-        <div className="chef">
-            <img src="/src/assets/images/chef.png"  className="" alt="Chef photo"/>
+    <div className="parent">
+      <div className="black-container" />
+      <div className="chef">
+        <img src="/src/assets/images/chef.png" className="" alt="Chef photo" />
+      </div>
+      <div className="gold-container">
+        <div className="text-container">
+          <h2>OUR CHEFS</h2>
+          <p>
+            Sed ut persipiciatis unde omnis iste natus error sit voluptatem
+            accusantium voluptatem a doloremque
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure do velit esse cilum
+          </p>
         </div>
-        <div className="gold-container">
-          <div className="text-container">
-            <h2>OUR CHEFS</h2><br/>
-            <p>Sed ut persipiciatis unde omnis iste natus error sit voluptatem accusantium voluptatem a doloremque</p> <br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure do velit esse cilum</p>
-          </div>
-        </div>
-      <div className ="blue-container"></div>
+      </div>
+      <div className="blue-container"></div>
     </div>
   );
 }
 
-export default RelativeSquareBlocks
+export default RelativeSquareBlocks;
