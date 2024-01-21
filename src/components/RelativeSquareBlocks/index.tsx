a50582b8cbb2537bfbd77c95b8ccbcc6a321b38d
adam <adam.lnenicka2@gmail.com>
2024-01-21 03:07:57 +0000

    square blocks update

--- src/components/RelativeSquareBlocks/index.tsx
+++ src/components/RelativeSquareBlocks/index.tsx
@@ -3,18 +3,17 @@ import "./RelativeSquareBlocks.scss";
 function RelativeSquareBlocks() {
   return (
     <div className="parent">
+      <div className="black-container" />
       <div className="chef">
         <img src="/src/assets/images/chef.png" className="" alt="Chef photo" />
       </div>
       <div className="gold-container">
         <div className="text-container">
           <h2>OUR CHEFS</h2>
-          <br />
           <p>
             Sed ut persipiciatis unde omnis iste natus error sit voluptatem
             accusantium voluptatem a doloremque
-          </p>{" "}
-          <br />
+          </p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
