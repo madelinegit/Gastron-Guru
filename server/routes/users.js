58ff9a12d10940220d73063286cb533052708cc6
vincent <vmcodes05@gmail.com>
2024-01-24 16:08:31 -0400

    initial server commit

---
+++ server/routes/users.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+module.exports = router;
