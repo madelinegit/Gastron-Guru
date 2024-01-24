1916e0dd73731e3af2581ce2def341d440a159af
Justin Nwankwo <nuhwainks@gmail.com>
2024-01-24 10:29:15 -0500

    benefits componenet

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-import Benefits from "../../components/Benefits/Index";
-=======
+import Benefits from "../../components/Benefits/index";
 import CookiesComponent from "../../components/CookiesComponent/CookiesComponent";
->>>>>>> upstream/develop
 import Featured from "../../components/Featured";
 import ForEveryOccasion from "../../components/ForEveryOccasion";
-import PopUpModal from "../../components/PopUpModal";
 import RelativeSquareBlocks from "../../components/RelativeSquareBlocks";
 
 const Home = () => {
   return (
-    <><PopUpModal/>
+    <>
       <Featured />
       <Benefits/>
       <ForEveryOccasion />
