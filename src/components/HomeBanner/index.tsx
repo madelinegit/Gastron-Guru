ec22006f1ab01110088f2f6b22cf679cf1c08281
Demi Hayashi <demi.h@me.com>
2024-01-25 11:00:44 -0800

    create HomeBanner component; set up initial structure & css

---
+++ src/components/HomeBanner/index.tsx
@@ -0,0 +1,14 @@
+import './HomeBanner.scss'
+
+const HomeBanner = () => {
+  return (
+    <section className="home-banner-container">
+      <div className="home">
+        <h1>Your Culinary Journey...</h1>
+        <button className="button-primary">Explore</button>
+      </div>
+    </section>
+  );
+};
+
+export default HomeBanner;
