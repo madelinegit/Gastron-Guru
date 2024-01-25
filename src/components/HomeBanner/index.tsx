d8f4e99e5e8a7e4809984c3dbea9975db7d2e5e4
Demi Hayashi <demi.h@me.com>
2024-01-25 12:02:23 -0800

    implement bg image for HomeBanner; update css & media queries

--- src/components/HomeBanner/index.tsx
+++ src/components/HomeBanner/index.tsx
@@ -1,11 +1,17 @@
-import './HomeBanner.scss'
+import './HomeBanner.scss';
 
 const HomeBanner = () => {
   return (
     <section className="home-banner-container">
+      <div className="overlay"></div>
       <div className="home">
+
         <h1>Your Culinary Journey...</h1>
-        <button className="button-primary">Explore</button>
+        
+        <a href="/chefs-database">
+          <button className="button-primary">Explore</button>
+        </a>
+
       </div>
     </section>
   );
