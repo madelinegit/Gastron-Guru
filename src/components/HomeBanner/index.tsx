cd11f6bbb37be4a30d7367ce1ef7b20f841a0cd0
Demi Hayashi <demi.h@me.com>
2024-01-29 09:23:46 -0800

    resolve merge conflicts

--- src/components/HomeBanner/index.tsx
+++ src/components/HomeBanner/index.tsx
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
+import NarrowContainer from '../NarrowContainer';
 import './HomeBanner.scss';
 
 const HomeBanner = () => {
   return (
     <section className="home-banner-container">
       <div className="overlay"></div>
-      <div className="home">
+      <NarrowContainer>
+        <div className="home">
 
-        <h1>Your Culinary Journey...</h1>
-        
-        <Link to="/chefs-database">
-          <button className="button-primary">Explore</button>
-        </Link>
-
-      </div>
+          <h1>Your Culinary Journey...</h1>
+            
+          <Link to="/chefs-database">
+            <button className="button-primary">Explore</button>
+          </Link>
+        </div>
+      </NarrowContainer>
     </section>
   );
 };
