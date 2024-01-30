f36ca6de1aa4504be0de1f28410d06e9b535c298
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-01-30 16:23:31 -0300

    refactor: rendering UserRegistrationForm as a component

--- src/pages/UserRegistration/index.tsx
+++ src/pages/UserRegistration/index.tsx
@@ -1,15 +1,7 @@
 import { UserRegistrationForm } from '../../components/UserRegistrationForm';
-import '../../components/UserRegistrationForm/UserRegistrationForm.scss';
 
 const UserRegistration: React.FC = () => {
-  return (
-    <main className="user-registration-container">
-      <div className="wrapper">
-        <h3>Register</h3>
-        <UserRegistrationForm />
-      </div>
-    </main>
-  );
+  return <UserRegistrationForm />;
 };
 
 export default UserRegistration;
