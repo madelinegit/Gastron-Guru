78782706f3728c73250bce0dc8b526a7bab39177
madelinegit <120407458+madelinegit@users.noreply.github.com>
2024-02-07 13:36:16 -0600

    update PR req

--- src/pages/ChefsDatabase/index.tsx
+++ src/pages/ChefsDatabase/index.tsx
@@ -1,19 +1,27 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+import ArrowButton from '../../components/Buttons/ArrowButton';
+import ChefCards from '../../components/ChefCards';
+import { ChefDataProps } from '../../components/ChefCards/types';
+import ChefDetail from '../../components/ChefDetail/ChefDetail';
+import SwitchInput from '../../components/Inputs/SwitchInput';
+import { LoadingSpinner } from '../../components/LoadingSpinner';
+import Map from '../../components/Map/Map';
+import MockNarrowContainer from '../../components/Modal/MockNarrowContainer';
+import Modal from '../../components/Modal/Modal';
+import ModalCard from '../../components/Modal/ModalCard';
+import SearchBar from '../../components/SearchBar';
+import SearchBarWrapper from '../../components/SearchBar/SearchBarWrapper';
+import useChef from '../../utils/Api';
+import { modalData } from '../../utils/Data';
 import {
-  useWindowResize,
-  useSwitchToggle,
+  useCardExpansion,
   useCheckboxToggle,
   useModal,
-  useCardExpansion,
-} from "../../utils/helpers";
-import { modalData } from "../../utils/Data";
-import Map from "../../components/Map/Map";
-import { LoadingSpinner } from "../../components/LoadingSpinner";
-import useChefsDatabaseEffects from "./useChefsDatabaseEffects";
-import { ChefDataProps } from "../../components/ChefCards/types";
-import SearchBarWrapper from "../../components/SearchBar/SearchBarWrapper";
-import useChef from "../../utils/Api";
-
+  useSwitchToggle,
+  useWindowResize,
+} from '../../utils/helpers';
+import useChefsDatabaseEffects from './useChefsDatabaseEffects';
+import '../../components/LoadingSpinner/LoadingSpinner.scss';
 
 const ChefsDatabase = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -34,15 +42,21 @@ const ChefsDatabase = () => {
   );
   const isScrollEnabled = isSwitchChecked || isOverrideActive || detailsShowing;
 
+  const [activeCard, setActiveCard] = useState<number>(0);
+
+  const onCardClick = (index: number) => {
+    setActiveCard(index);
+  };
+
   useEffect(() => {
     try {
       chefData;
     } catch (error) {
-      console.log("An error occurred...");
+      console.log('An error occurred...');
       setLoading(true);
     }
     return () => {
-      console.log("Done!");
+      console.log('Done!');
       setLoading(false);
     };
   }, [chefData]);
@@ -51,11 +65,29 @@ const ChefsDatabase = () => {
   useChefsDatabaseEffects({ chefData, setSortChefCards });
 
   return (
-    <div
-      style={
-        loading ? { overflow: "hidden", position: "fixed", width: "100vw" } : {}
-      }
-    >
+    <div className={`${loading && 'spinner-wrapper'}`}>
+      <ArrowButton handleBtnToggle={handleModalToggle} state={showModal} />
+      {showModal && (
+        <Modal>
+          <MockNarrowContainer>
+            {' '}
+            {modalData.map((card) => (
+              <ModalCard
+                key={card.label}
+                {...card}
+                isExpanded={expandedCards.includes(card.label)}
+                onToggleExpansion={() => toggleCardExpansion(card.label)}
+              />
+            ))}
+          </MockNarrowContainer>
+        </Modal>
+      )}
+      <SwitchInput
+        isChecked={(isSwitchChecked && !isOverrideActive) || isOverrideActive}
+        onToggle={handleSwitchToggle}
+      />
+      <SearchBar />
+
       <SearchBarWrapper
         handleCheckboxToggle={handleCheckboxToggle}
         handleSwitchToggle={handleSwitchToggle}
@@ -71,8 +103,13 @@ const ChefsDatabase = () => {
 
       {loading && <LoadingSpinner />}
 
-
-
+      <ChefCards
+        chefData={chefData}
+        isScrollEnabled={isScrollEnabled}
+        onCardClick={onCardClick}
+        activeCard={activeCard}
+      />
+      <ChefDetail activeCard={activeCard} />
 
       {isScrollEnabled && (
         <section>
