2104bd65408637272873f61901457973256af10f
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-02-08 17:10:46 -0300

    fix: make New York the default destination

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const Map = () => {
   const mapContainerStyle = {
-    width: '435px',
-    height: '461px',
-  }
+    width: "435px",
+    height: "461px",
+  };
 
   //please talk to Adam for key (need to set up .env)
-  const GOOGLE_MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY!
+  const GOOGLE_MAP_API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY!;
 
   return (
     <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center={{ lat: 41.3851, lng: 2.1734 }}
+        center={{ lat: 40.73061, lng: -73.935242 }}
         zoom={10}
       />
     </LoadScript>
-  )
-}
-export default Map
+  );
+};
+export default Map;
