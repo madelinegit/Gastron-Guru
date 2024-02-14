dd33d7d49ac2ebbf2e23cfa76a2835166fa14728
demivlkv <demi.h@me.com>
2024-02-14 15:26:53 -0800

    create dropdown menu for reviews section

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronRight, faStar } from '@fortawesome/free-solid-svg-icons';
 import './Rating.scss';
 
 const Rating = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setOpen(!open);
+
   return (
     <div className="rating-container">
       <div className="rating-sidebar">
@@ -11,7 +16,7 @@ const Rating = () => {
           <h3>{/* STARS */} 4.3 out of 5</h3>
           <p>432 global ratings</p>
 
-          {/* RATINGS */}
+          {/* ALL RATINGS */}
 
           <div className="sidebar-info">
             <FontAwesomeIcon icon={faChevronDown} size="xs" />
@@ -21,7 +26,7 @@ const Rating = () => {
           </div>
         </section>
 
-        <section>
+        <section className="review-chef">
           <h2>Review this chef</h2>
           <p>Share your thoughts with other customers</p>
           <button className="button-primary">
@@ -43,27 +48,68 @@ const Rating = () => {
             {/* INSERT IMAGE CAROUSEL */}
           </div>
 
-          {/* INSERT DROPDOWN HERE */}
-          <h2>Top reviews from the United States</h2>
-          <div className="user-info">
-            <img src="https://placekitten.com/200/300" alt="User Avatar" />
-            <p>E. Jones</p>
+          {/* DROPDOWN MENU */}
+          <div className="dropdown">
+            <button onClick={handleOpen} className="dropdown-btn">
+              Top Reviews
+              <FontAwesomeIcon icon={faChevronDown} size="sm" />
+            </button>
+            {open ? (
+              <ul className="dropdown-menu">
+                <li className="menu-item"><button>Option 2</button></li>
+                <li className="menu-item"><button>Option 3</button></li>
+                <li className="menu-item"><button>Option 4</button></li>
+              </ul>
+            ) : null}
           </div>
-          {/* INSERT STAR RATING */} <p>Highly recommend</p>
-          <p className="blue-text">Reviewed in the United States on October 26, 2023</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Volutpat maecenas volutpat blandit aliquam. Justo nec ultrices dui sapien eget mi proin sed. Dictum non consectetur a erat nam at lectus urna. Facilisis magna etiam tempor orci eu lobortis. Ornare suspendisse sed nisi lacus sed viverra tellus in hac.
-          </p>
 
-          <div className="user-info">
-            <img src="https://placekitten.com/200/287" alt="User Avatar" />
-            <p>Laura M.</p>
-          </div>
-          {/* INSERT STAR RATING */} <p>Highly recommend</p>
-          <p className="blue-text">Reviewed in the United States on December 18, 2023</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Volutpat maecenas volutpat blandit aliquam. Justo nec ultrices dui sapien eget mi proin sed. Dictum non consectetur a erat nam at lectus urna. Facilisis magna etiam tempor orci eu lobortis. Ornare suspendisse sed nisi lacus sed viverra tellus in hac.
-          </p>
+          <h2>Top reviews from the United States</h2>
+          <article className="reviews">
+            <div className="user-info">
+              <img src="https://placekitten.com/200/300" alt="User Avatar" />
+              <p>E. Jones</p>
+            </div>
+
+            {/* USER STAR RATING */}
+            <div className="user-rating">
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <strong>Highly recommend</strong>
+            </div>
+
+            <p className="blue-text">
+              Reviewed in the United States on October 26, 2023
+            </p>
+            <p className="review-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Volutpat maecenas volutpat blandit aliquam. Justo nec ultrices dui sapien eget mi proin sed. Dictum non consectetur a erat nam at lectus urna. Facilisis magna etiam tempor orci eu lobortis. Ornare suspendisse sed nisi lacus sed viverra tellus in hac.
+            </p>
+          </article>
+
+          <article>
+            <div className="user-info">
+              <img src="https://placekitten.com/200/287" alt="User Avatar" />
+              <p>Laura M.</p>
+            </div>
+
+            <div className="user-rating">
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <strong>Wonderful food and service!</strong>
+            </div>
+
+            <p className="blue-text">
+              Reviewed in the United States on December 18, 2023
+            </p>
+            <p className="review-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Volutpat maecenas volutpat blandit aliquam. Justo nec ultrices dui sapien eget mi proin sed. Dictum non consectetur a erat nam at lectus urna. Facilisis magna etiam tempor orci eu lobortis. Ornare suspendisse sed nisi lacus sed viverra tellus in hac.
+            </p>
+          </article>
         </main>
       </div>
     </div>
