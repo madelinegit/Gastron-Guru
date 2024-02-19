3a60b6b267e162eaaa4cd5d65a9bf5ba6a029c90
demivlkv <demi.h@me.com>
2024-02-19 10:33:54 -0800

    minor code cleanup

--- src/pages/Rating/index.tsx
+++ src/pages/Rating/index.tsx
@@ -13,6 +13,7 @@ const Rating = () => {
   const customerReviews = ReviewsData;
   const [open, setOpen] = useState<boolean>(false);
 
+  // TOGGLE DROPDOWN MENU
   const handleOpen = () => setOpen(!open);
 
   return (
@@ -50,63 +51,61 @@ const Rating = () => {
         </section>
       </div>
 
-      <div className='rating-main'>
-        <main>
-          <h2>Customers say</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+      <main className='rating-main'>
+        <h2>Customers say</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
 
-          <div className='carousel'>
-            <h2>Reviews with images</h2>
-            <a href='#'>See all photos <FontAwesomeIcon icon={faChevronRight} size='2xs' /></a>
-            
-            {/* INSERT IMAGE CAROUSEL HERE */}
-          </div>
+        <div className='image-carousel'>
+          <h2>Reviews with images</h2>
+          <a href='#'>See all photos <FontAwesomeIcon icon={faChevronRight} size='2xs' /></a>
 
-          {/* DROPDOWN MENU */}
-          <div className='dropdown'>
-            <button onClick={handleOpen} className='dropdown-btn'>
-              Top Reviews
-              {open ? <FontAwesomeIcon icon={faChevronUp} size='sm' /> : <FontAwesomeIcon icon={faChevronDown} size='sm' />}
-            </button>
-            {open && (
-              <ul className='dropdown-menu'>
-                <li className='menu-item'><button>Most Recent</button></li>
-                <li className='menu-item'><button>Oldest</button></li>
-                <li className='menu-item'><button>Most Helpful</button></li>
-                <li className='menu-item'><button>Highest Rating</button></li>
-                <li className='menu-item'><button>Lowest Rating</button></li>
-                <li className='menu-item'><button>Least Helpful</button></li>
-              </ul>
-            )}
-          </div>
+          {/* INSERT IMAGE CAROUSEL HERE */}
+        </div>
 
-          <h2>Top reviews from the United States</h2>
-          {/* USING HARDCODED MOCK DATA UNTIL CONNECTED TO DATABASE */}
-          {customerReviews.map((review) => (
-            <article className='reviews'>
-              <div className='user-info'>
-                <img src={review.avatar} alt={review.username} />
-                <p>{review.username}</p>
-              </div>
+        {/* DROPDOWN MENU */}
+        <div className='dropdown'>
+          <button onClick={handleOpen} className='dropdown-btn'>
+            Top Reviews
+            {open ? <FontAwesomeIcon icon={faChevronUp} size='sm' /> : <FontAwesomeIcon icon={faChevronDown} size='sm' />}
+          </button>
+          {open && (
+            <ul className='dropdown-menu'>
+              {/* FILLER CONTENT; UPDATE LATER */}
+              <li className='menu-item'><button>Most Recent</button></li>
+              <li className='menu-item'><button>Oldest</button></li>
+              <li className='menu-item'><button>Most Helpful</button></li>
+              <li className='menu-item'><button>Highest Rating</button></li>
+              <li className='menu-item'><button>Lowest Rating</button></li>
+              <li className='menu-item'><button>Least Helpful</button></li>
+            </ul>
+          )}
+        </div>
 
-              {/* USER STAR RATING */}
-              <div className='user-rating'>
+        <h2>Top reviews from the United States</h2>
+        {/* USING HARDCODED MOCK DATA UNTIL CONNECTED TO DATABASE */}
+        {customerReviews.map((review) => (
+          <article className='reviews'>
+            <div className='user-info'>
+              <img src={review.avatar} alt={review.username} />
+              <p>{review.username}</p>
+            </div>
+
+            <div className='user-rating'>
               <RatingStars rating={review.starRating} />
-                <strong>{review.reviewTitle}</strong>
-              </div>
+              <strong>{review.reviewTitle}</strong>
+            </div>
 
-              <p className='blue-text'>
-                Reviewed in {review.country} on {review.date}
-              </p>
-              <p className='review-text'>
-                {review.userReview}
-              </p>
-            </article>
-          ))}
-        </main>
-      </div>
+            <p className='blue-text'>
+              Reviewed in {review.country} on {review.date}
+            </p>
+            <p className='review-text'>
+              {review.userReview}
+            </p>
+          </article>
+        ))}
+      </main>
     </div>
   );
 };
